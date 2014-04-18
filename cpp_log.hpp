b8d5a6de710c4438f1edd8ec8ba15a9fb5f90738
yukipyon <wolfvbear@gmail.com>
2014-04-18 11:55:05 +0100

    Added unary file logging.

--- cpp_log.hpp
+++ cpp_log.hpp
@@ -2,15 +2,36 @@
 #define CPP_LOG
 
 #include <ostream>
+#include <iostream>
+
 struct logger {
 
-	std::ostream &ss;
-	logger(std::ostream &s) : ss(s) {}
+	#ifdef CPP_LOG_FILE
+	std::ofstream ofs;
+	#endif
+
+	logger()
+	{
+		#ifdef CPP_LOG_FILE
+		ofs.open(CPP_LOG_FILE);
+		#endif
+	}
+
+	~logger()
+	{
+		#ifdef CPP_LOG_FILE
+		ofs.close();
+		#endif
+	}
 
 	template <typename t>
 	logger& operator<<(const t s)
 	{
-		ss << s;
+		#ifdef CPP_LOG_FILE
+		ofs << s;
+		#endif
+
+		//ss[0] << s;
 		return *this;
 	}
 };
--- unit_test.cpp
+++ unit_test.cpp
@@ -1,11 +1,15 @@
 #define BOOST_TEST_MODULE example
 #include <boost/test/included/unit_test.hpp>
+
+#define CPP_LOG_FILE "output"
+
 #include "cpp_log.hpp"
 #include <iostream>
 
+
 BOOST_AUTO_TEST_CASE(a)
 {
-	logger log(std::cout);
+	logger log;
 	log << "hello " << 6;
 
 }
