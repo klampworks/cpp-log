f2d70100c11329d5e51f7ac45d5fda5c8ef93bbf
yukipyon <wolfvbear@gmail.com>
2014-04-19 10:09:37 +0100

    Renamed second solution.

---
+++ cpp_log.hpp
@@ -0,0 +1,139 @@
+#ifndef CPP_LOG2
+#define CPP_LOG2
+
+/* TODO symbol log is already defined in mathcalls.h. */
+namespace clog {
+
+#include <sstream>
+#include <iostream>
+#include <ctime>
+
+struct logger2 {
+
+	std::stringstream ss;
+	const char *level;
+
+	std::string now()
+	{
+		time_t t = time(NULL);
+		struct tm *tm_info;
+		tm_info = localtime(&t);
+
+		char buf[20];
+		strftime(buf, 20, "%F %T", tm_info);
+		return std::string(buf);
+	}
+
+	logger2(const char *llevel)
+	{
+		level = llevel;
+	}
+
+	void flush(std::ostream &out)
+	{
+		out << level << ": " << now() << " Prefix " 
+			<< ss.str() << std::endl;
+	}
+
+	virtual ~logger2()
+	{
+	}
+
+	template <typename t>
+	logger2& operator<<(const t s)
+	{
+		ss << s;
+		return *this;
+	}
+};
+
+/* TODO Copy and pasted code. */
+struct err : public logger2 {
+
+	#ifdef CPP_LOG_ERR_FILE
+	std::ofstream ofs;
+	#endif
+
+	err() : logger2("Error") {
+		#ifdef CPP_LOG_ERR_FILE
+		ofs.open(CPP_LOG_ERR_FILE, 
+			std::ios_base::app | std::ios_base::out);
+		#endif
+	};
+
+	~err() {
+		#ifdef CPP_LOG_ERR_COUT
+		flush(std::cout);
+		#endif
+
+		#ifdef CPP_LOG_ERR_CERR
+		flush(std::cerr);
+		#endif
+
+		#ifdef CPP_LOG_ERR_FILE
+		flush(ofs);
+		ofs.close();
+		#endif
+	}
+};
+
+struct warn : public logger2 {
+
+	#ifdef CPP_LOG_WARN_FILE
+	std::ofstream ofs;
+	#endif
+
+	warn() : logger2("Warning") {
+		#ifdef CPP_LOG_WARN_FILE
+		ofs.open(CPP_LOG_WARN_FILE, 
+			std::ios_base::app | std::ios_base::out);
+		#endif
+	};
+
+	~warn() {
+		#ifdef CPP_LOG_WARN_COUT
+		flush(std::cout);
+		#endif
+
+		#ifdef CPP_LOG_WARN_CERR
+		flush(std::cerr);
+		#endif
+
+		#ifdef CPP_LOG_WARN_FILE
+		flush(ofs);
+		ofs.close();
+		#endif
+	}
+};
+
+struct info : public logger2 {
+
+	#ifdef CPP_LOG_INFO_FILE
+	std::ofstream ofs;
+	#endif
+
+	info() : logger2("Info") {
+		#ifdef CPP_LOG_INFO_FILE
+		ofs.open(CPP_LOG_INFO_FILE, 
+			std::ios_base::app | std::ios_base::out);
+		#endif
+	};
+
+	~info() {
+		#ifdef CPP_LOG_INFO_COUT
+		flush(std::cout);
+		#endif
+
+		#ifdef CPP_LOG_INFO_CERR
+		flush(std::cerr);
+		#endif
+
+		#ifdef CPP_LOG_INFO_FILE
+		flush(ofs);
+		ofs.close();
+		#endif
+	}
+};
+
+}
+#endif
--- unit_test.cpp
+++ unit_test.cpp
@@ -1,7 +1,6 @@
 #define BOOST_TEST_MODULE example
 #include <boost/test/included/unit_test.hpp>
 
-//#define CPP_LOG_FILE "output"
 #define CPP_LOG_COUT 
 #define CPP_LOG_CERR 
 #define CPP_LOG_ERR_COUT
@@ -9,11 +8,8 @@
 #define CPP_LOG_ERR_FILE "output"
 
 #include "cpp_log.hpp"
-#include "cpp_log2.hpp"
 
 BOOST_AUTO_TEST_CASE(a)
 {
-	logger log;
-	error() << "hello " << 6 << std::hex << 666;
-
+	clog::err() << "hello " << 66 << " " << std::hex << 66;
 }
